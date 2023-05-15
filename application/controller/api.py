d9bf66bc01fcab48b80ddee2150fb91e72b1723b
a05031113 <a05031113@gmail.com>
2023-05-15 23:44:50 +0800

    add ga4 Measurement Protocol

--- application/controller/api.py
+++ application/controller/api.py
@@ -1,6 +1,12 @@
 from flask import *
 from PIL import Image
+from dotenv import load_dotenv
+import os
 import io
+import uuid
+import requests
+load_dotenv()
+
 
 api = Blueprint('api', __name__)
 
@@ -8,6 +14,8 @@ api = Blueprint('api', __name__)
 @api.route('/api/image', methods=['GET'])
 def get_png():
     try:
+        track_api_execution()
+
         width = int(request.args.get('width'))
         height = int(request.args.get('height'))
 
@@ -18,3 +26,24 @@ def get_png():
         return send_file(filename, mimetype='image/png')
     except:
         return jsonify({"error": True, "message": SyntaxError}), 500
+
+
+def track_api_execution():
+    api_secret = os.getenv(api_secret)
+    measurement_id = os.getenv(measurement_id)
+    url = 'https://www.google-analytics.com/mp/collect?measurement_id=' + \
+        api_secret+'&api_secret='+measurement_id
+
+    payload = {
+        'client_id': uuid.uuid4(),
+        'events': [{
+            'name': 'qsearch',
+            'params': {}
+        }]
+    }
+    headers = {'content-type': 'application/json'}
+
+    response = requests.post(url, data=json.dumps(payload), headers=headers)
+
+    if response.status_code != 200:
+        print('Failed to send API execution data to Google Analytics.')
